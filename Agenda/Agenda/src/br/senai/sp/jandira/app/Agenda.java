479792047ba897cfd721c0903736f12cf5ea3f85
unknown <leonardp_cavalcante.07@hotmail.com>
2019-01-02 23:32:36 -0200

    First Commit

---
+++ Agenda/Agenda/src/br/senai/sp/jandira/app/Agenda.java
@@ -0,0 +1,17 @@
+package br.senai.sp.jandira.app;
+
+import br.senai.sp.jandira.view.FrmAgenda;
+
+public class Agenda {
+
+	public static void main(String[] args) {
+		
+		FrmAgenda agenda = new FrmAgenda();
+		agenda.setVisible(true);
+		
+		
+		
+
+	}
+
+}
